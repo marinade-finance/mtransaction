40053aff32db2978baafc44d34bfe2e694cc7ff3
Alexandru Stefan <alex@ndru.dev>
2022-09-18 17:24:41 +0200

    feat: be able to reopen stream if randomly closes

--- client/mconnector.js
+++ client/mconnector.js
@@ -1,19 +1,25 @@
-var grpc = require('@grpc/grpc-js');
 // const fs = require('fs');
 
-var path = require("path");
-var PROTO_PATH = path.join(__dirname, '..', 'proto', 'mtransaction.proto');
+const path = require("path");
+const PROTO_PATH = path.join(__dirname, '..', 'proto', 'mtransaction.proto');
 
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {keepCase: true});
+
+const grpc = require('@grpc/grpc-js');
+const validatorProto = grpc.loadPackageDefinition(packageDefinition).validator;
+
+function connect() {
+    const mtransactionClient = new validatorProto.MTransaction('192.168.200.176:50051', grpc.credentials.createInsecure());
+    const call = mtransactionClient.EchoStream({message: 'Listening for transactions'});
+    call.on('data', (response) => {
+        console.log(response);
     });
-var validator_proto = grpc.loadPackageDefinition(packageDefinition).validator;
+    call.on('end',function(){
+        console.log("Stream ended")
+        connect();
+    });
+}
 
 function main() {
     // const ssl_creds = grpc.credentials.createSsl(
@@ -22,13 +28,7 @@ function main() {
     //     fs.readFileSync(path.join(__dirname, '..', 'cert', 'client-cert.cer')),
     // );
     // var client = new validator_proto.MTransaction('192.168.200.176:50051', ssl_creds);
-    var client = new validator_proto.MTransaction('192.168.200.176:50051', grpc.credentials.createInsecure());
-
-    client.EchoStream({message: 'world'}, function(err, response) {
-        if (err) throw err;
-        console.log('Greeting:', response.message);
-        // client.close();
-    });
+    connect();
 }
 
 main();
