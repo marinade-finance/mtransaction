eccc8128b989922cb90893b6d90a34dbfdb2b7b9
Alexandru Stefan <alex@ndru.dev>
2022-09-18 13:13:20 +0200

    fix: add syntax for ssl credentials

--- client/mconnector.js
+++ client/mconnector.js
@@ -1,4 +1,5 @@
 var grpc = require('@grpc/grpc-js');
+// const fs = require('fs');
 
 var path = require("path");
 var PROTO_PATH = path.join(__dirname, '..', 'proto', 'mtransaction.proto');
@@ -15,14 +16,19 @@ var packageDefinition = protoLoader.loadSync(
 var validator_proto = grpc.loadPackageDefinition(packageDefinition).validator;
 
 function main() {
-  var client = new validator_proto.MTransaction('https://192.168.200.176:50051/',
-                                       grpc.credentials.createInsecure());
+    // const ssl_creds = grpc.credentials.createSsl(
+    //     fs.readFileSync(path.join(__dirname, '..', 'cert', 'ca-cert.cer')), 
+    //     fs.readFileSync(path.join(__dirname, '..', 'cert', 'client-key.cer')), 
+    //     fs.readFileSync(path.join(__dirname, '..', 'cert', 'client-cert.cer')),
+    // );
+    // var client = new validator_proto.MTransaction('192.168.200.176:50051', ssl_creds);
+    var client = new validator_proto.MTransaction('192.168.200.176:50051', grpc.credentials.createInsecure());
 
-  client.Echo({name: user}, function(err, response) {
-    if (err) throw err;
-    console.log('Greeting:', response.message);
-    client.close();
-  });
+    client.Echo({message: 'world'}, function(err, response) {
+        if (err) throw err;
+        console.log('Greeting:', response.message);
+        client.close();
+    });
 }
 
 main();
