fe99911fd6c0209b462774965a443d4a9e3f3194
Alexandru Stefan <alex@ndru.dev>
2022-09-21 20:14:16 +0200

    feat: add restart mechanism

--- client/mconnector.js
+++ client/mconnector.js
@@ -10,21 +10,35 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {keepCase: true});
 const grpc = require('@grpc/grpc-js');
 const validatorProto = grpc.loadPackageDefinition(packageDefinition).validator;
 
+function restart(millisecondsToWait, r, typ) {
+    console.log(r, "Stream ended", millisecondsToWait, typ);
+    setTimeout(() => {
+        connect();
+    }, millisecondsToWait);
+}
+
 function connect() {
+    let millisecondsToWait = 500;
+    const r = (Math.random() + 1).toString(36).substring(7);
     const ssl_creds = grpc.credentials.createSsl(
         fs.readFileSync(path.join(__dirname, '..', 'certs', 'localhost.cert')),
     );
     const mtransactionClient = new validatorProto.MTransaction(`localhost:50051`, ssl_creds);
     const call = mtransactionClient.TxStream({message: 'Listening for transactions'}, (err, message) => {
-        console.log(err, message);
+        console.log(r, err, message);
     });
     call.on('data', ({ data }) => {
         const tx = web3.Transaction.populate(web3.Message.from(Buffer.from(data, 'base64')))
-        console.log(data);
+        console.log(r, data);
     });
-    call.on('end',function(){
-        console.log("Stream ended")
-        connect();
+    call.on('end', () => {
+        millisecondsToWait += millisecondsToWait;
+        mtransactionClient.close();
+        console.log(r, "connection end")
+        restart(millisecondsToWait);
+    });
+    call.on('error', (err) => {
+        console.error(r, err);
     });
 }
 
