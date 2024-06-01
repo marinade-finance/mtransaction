25741b3959fffb50954025268e4a7ee315d5119a
janlegner <jan.legner@gmail.com>
2024-06-01 16:57:24 +0300

    v0.0.13-beta

--- client/main.rs
+++ client/main.rs
@@ -25,7 +25,7 @@ use std::{collections::HashMap, io::Error};
 
 use futures::stream::StreamExt;
 
-pub const VERSION: &str = "rust-0.0.7-beta";
+pub const VERSION: &str = "rust-0.0.13-beta";
 
 // Linearly delay retries up to 60 seconds
 const GRPC_RECONNECT_DELAY: u64 = 1_000;
